c8095833427ed55f7b342987ea801f29aa9cf72d
unknown <mydulcse62.niter@gmail.com>
2024-03-22 11:23:58 +0600

    fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <footer className="footer-center flex justify-center items-center h-14  bg-base-200  rounded">
-    <p >Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+    <p >Copyright © 2024 - All right reserved by Md mydul Islam-2024</p>
 </footer>
   );
 };
